3b7e3aab7cf913046fc8edfceb2b00c1471cde2a
Ilya Andriyanov <ilyaandry35@gmail.com>
2024-10-24 15:11:29 +0300

    feature: moved Handlebars init out of index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
+import { handlebarsInit } from '/src/modules/Handlebars/Handlebars.js';
 import router from '/src/modules/Router/Router.js';
-import { resolveStatic, resolveUrl } from './modules/UrlUtils/UrlUtils.js';
 import appState from './modules/AppState/AppState.js';
 import { LoginPage } from './Pages/LoginPage/LoginPage.js';
 import { RegistrationPage } from './Pages/RegistrationPage/RegistrationPage.js';
 import { VacanciesPage } from './Pages/VacanciesPage/VacanciesPage.js';
 
-Handlebars.registerHelper('static', resolveStatic);
-Handlebars.registerHelper('url', resolveUrl);
-
-Handlebars.registerPartial('header', Handlebars.templates['header.hbs']);
-Handlebars.registerPartial('login-form', Handlebars.templates['login-form.hbs']);
-Handlebars.registerPartial('notification', Handlebars.templates['notification.hbs']);
-Handlebars.registerPartial(
-  'employer-registration-form',
-  Handlebars.templates['employer-registration-form.hbs'],
-);
-Handlebars.registerPartial(
-  'applicant-registration-form',
-  Handlebars.templates['applicant-registration-form.hbs'],
-);
-Handlebars.registerPartial(
-  'user-type-radiogroup',
-  Handlebars.templates['user-type-radiogroup.hbs'],
-);
-Handlebars.registerPartial('validated-input', Handlebars.templates['validated-input.hbs']);
-Handlebars.registerPartial('validated-textarea', Handlebars.templates['validated-textarea.hbs']);
+handlebarsInit();
 
 router.addRoute('/', VacanciesPage);
 router.addRoute('/login', LoginPage);
