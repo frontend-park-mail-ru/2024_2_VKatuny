37542343fe2505180473a462feec49f662f5a161
Ilya Andriyanov <ilyaandry35@gmail.com>
2024-11-11 15:27:00 +0300

    feature: babel transpiler

--- webpack.common.js
+++ webpack.common.js
@@ -28,6 +28,17 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: '> 0.25%, not dead',
+            presets: [['@babel/preset-env']],
+          },
+        },
+      },
     ],
   },
   entry: {
